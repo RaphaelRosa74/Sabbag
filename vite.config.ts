d0521b194f9758dab145c58ff3e7ed244bcad977
RaphaelRosa74 <raphafaria7@hmail.com>
2026-03-31 16:32:39 -0300

    config de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: mode === "development" ? "" : "/Sabbag/",
+  base: mode === "development" ? "" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
